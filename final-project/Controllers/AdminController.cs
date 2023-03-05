d827e72bf301ec9c033d68b125b2a9fe6e7635f7
Victor147 <viktorivanov_e18@schoolmath.eu>
2023-03-05 11:13:07 +0200

    Admin panel added

---
+++ final-project/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace final_project.Controllers;
+
+public class AdminController : Controller
+{
+    // GET
+    public IActionResult Panel()
+    {
+        return View();
+    }
+}
